e66a90e3faeabc34b450c88ac938c94fcb6fa900
Francesco de rosa <fderosa067@gmail.com>
2022-08-16 23:33:06 -0300

    primer desafio react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+
+        
         <p>
           Antonella Estética
         </p>
