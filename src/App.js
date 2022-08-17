e5116af879567cdd1b66978996bc1544489eda3c
Francesco de rosa <fderosa067@gmail.com>
2022-08-16 22:25:44 -0300

    CREO finalizado.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Antonella Estética
         </p>
-        
+        <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          Lear React
+        </a>
       </header>
     </div>
   );
